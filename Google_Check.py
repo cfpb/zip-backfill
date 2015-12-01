87c6714279209d2c78a5cd0b428a71b75b595680
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2015-12-01 10:48:36 -0500

    check now outputs results into a file instead of printing

--- Google_Check.py
+++ Google_Check.py
@@ -4,45 +4,41 @@ import argparse
 #command line parser
 parser = argparse.ArgumentParser(description = 'Backfills zip code to single file')
 parser.add_argument('input', help = 'OpenAddresses file with backfilled zip to check')
+parser.add_argument('output', help = 'file to output accuracy data')
 parser.add_argument('key', help = 'a file containing a valid google api key')
 args = parser.parse_args()
 
 key = open(args.key, 'r').read()
-
 url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
 url_post = '&key=' + key
 
-good = 0
-fail = 0
-not_found = 0
+#check address against google api
+def check_address(row):
+	parts = row.split(',')
+	#construct url with lat and lon and send request
+	result = (requests.get(url_pre + parts[1] + ',' + parts[0] + url_post)).json()
+	#if google finds point
+	if result['status'] == 'OK':
+		for piece in reversed(result['results'][0]['address_components']):
+			#look for post code
+			if piece['types'][0] == 'postal_code':
+				#check if post code is same as zip in file
+				if piece['long_name'] == parts[7]:
+					return 'Same'
+				else:
+					return piece['long_name']
+		#if no postcode
+		else:
+			return 'Not Found'
+	#if no result
+	else:
+		return 'Not Found'
 
+output = open(args.output, 'w')
 with open(args.input, 'r') as source:
 	for row in source:
-		parts = row.split(',')
-		#construct url with lat and lon and send request
-		check = requests.get(url_pre + parts[1] + ',' + parts[0] + url_post)
-		result = check.json()
-		#if google finds point
-		if result['status'] == 'OK':
-			for piece in result['results'][0]['address_components']:
-				#look for post code
-				if piece['types'][0] == 'postal_code':
-					#check if post code is same as zip in file
-					if piece['long_name'] == parts[7]:
-						print 'pass'
-						good += 1
-					else:
-						print 'fail'
-						fail += 1
-						print piece['long_name'] + '  ' + parts[7]
-					break
-			else:
-				print 'not_found'
-				not_found += 1
-		else:
-			print 'not found'
-			not_found += 1
+		if row[0] != '#': output.write(row.strip('\n') + ',' + check_address(row) + '\n')
+		else: output.write(row)
 
-print 'pass: ' + str(good)
-print 'fail: ' + str(fail)
-print 'not_found: ' + str(not_found)
+output.close()
+		
