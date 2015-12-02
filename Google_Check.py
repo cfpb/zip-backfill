164f1823040d582a41f997016467e18edbe3e196
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2015-12-02 11:50:42 -0500

    made it more functional and simpler

--- Google_Check.py
+++ Google_Check.py
@@ -9,38 +9,22 @@ parser.add_argument('key', help = 'a file containing a valid google api key')
 parser.add_argument('-r', '--reverse', help = 'use a reverse geocode search', action = 'store_true')
 args = parser.parse_args()
 
-key = open(args.key, 'r').read()
-url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
-url_post = '&key=' + key
-
 #check address against google api
-def check_address_reverse(parts, session):
+def check_address_reverse(parts, session, key):
 	url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
 	url_post = '&key=' + key
 	#construct url with lat and lon and send request
 	result = (session.get(url_pre + parts[1] + ',' + parts[0] + url_post)).json()
-	#if google finds point
-	if result['status'] == 'OK':
-		for piece in reversed(result['results'][0]['address_components']):
-			#look for post code
-			if piece['types'][0] == 'postal_code':
-				#check if post code is same as zip in file
-				if piece['long_name'] == parts[7]:
-					return 'Same'
-				else:
-					return piece['long_name']
-		#if no postcode
-		else:
-			return 'Not Found'
-	#if no result
-	else:
-		return 'Not Found'
+	return parse_response(result)
 
-def check_address(parts, session):
+def check_address(parts, session, key):
 	url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 	url_post = '&key=' + key
-	#construct url with lat and lon and send request
+	#construct url with address and send request
 	result = (session.get(url_pre + (parts[2] + ' ' + parts[3] + ', ' + parts[4] + ', ' + parts[6]).replace(' ', '+') + url_post)).json()
+	return parse_response(result)
+
+def parse_response(result):
 	#if google finds point
 	if result['status'] == 'OK':
 		for piece in reversed(result['results'][0]['address_components']):
@@ -58,13 +42,14 @@ def check_address(parts, session):
 	else:
 		return 'Not Found'
 
+key = open(args.key, 'r').read()
 with requests.Session() as google:
 	output = open(args.output, 'w')
 	with open(args.input, 'r') as source:
 		for row in source:
 			parts = (row.strip('\r\n')).split(',')
-			if row[0] != '#' and args.reverse: output.write(','.join(parts + [check_address_reverse(parts, google)]) + '\n')
-			elif row[0] != '#': output.write(','.join(parts + [check_address(parts, google)]) + '\n')
+			if row[0] != '#' and args.reverse: output.write(','.join(parts + [check_address_reverse(parts, google, key)]) + '\n')
+			elif row[0] != '#': output.write(','.join(parts + [check_address(parts, google, key)]) + '\n')
 			else: output.write(row)
 
 output.close()
