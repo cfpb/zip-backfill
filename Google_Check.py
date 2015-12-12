c5f8a1bd4df715a6a9d0ecf13e3e38f120c95d69
Kenneth Gudel <kenneth.gudel@cfpb.gov>
2015-12-12 16:26:38 -0500

    functionalized files to use them as modules. Added a file that uses all of the other functionalized files to run a test with a single command, running into a confusing bug which, will post as issue

--- Google_Check.py
+++ Google_Check.py
@@ -3,17 +3,6 @@ Runs through an OpenAddresses formatted file and checks the zip codes present ag
 '''
 
 import requests
-import argparse
-
-#command line parser
-parser = argparse.ArgumentParser(description = 'Checks an OpenAddresses formatted file against a web API')
-parser.add_argument('input', help = 'OpenAddresses file with backfilled zip to check')
-parser.add_argument('output', help = 'file to output accuracy data')
-parser.add_argument('key', help = 'a file containing a valid google api key')
-parser.add_argument('-r', '--reverse', help = 'use a reverse geocode search', action = 'store_true')
-parser.add_argument('-z', '--zip', help = 'search with zip code', action = 'store_true')
-parser.add_argument('-m', '--mapbox', help = 'geocode from the mapbox API', action = 'store_true')
-args = parser.parse_args()
 
 #check address against google api
 def check_address_reverse(parts, session, key):
@@ -21,21 +10,21 @@ def check_address_reverse(parts, session, key):
 	url_post = '&key=' + key
 	#construct url with lat and lon and send request
 	result = (session.get(url_pre + parts[1] + ',' + parts[0] + url_post)).json()
-	return parse_response(result)
+	return parse_google_response(result, parts)
 
 def check_address(parts, session, key):
 	url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 	url_post = '&key=' + key
 	#construct url with address and send request
 	result = (session.get(url_pre + (parts[2] + ' ' + parts[3] + ', ' + parts[4] + ', ' + parts[6]).replace(' ', '+') + url_post)).json()
-	return parse_response(result)
+	return parse_google_response(result, parts)
 
 def check_address_zip(parts, session, key):
 	url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 	url_post = '&key=' + key
 	#construct url with address and send request
 	result = (session.get(url_pre + (parts[2] + ' ' + parts[3] + ', ' + parts[4] + ', ' + parts[6] + ' ' + parts[7]).replace(' ', '+') + url_post)).json()
-	return parse_response(result)
+	return parse_google_response(result, parts)
 
 def check_address_mapbox(parts, session, key):
 	url_pre = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
@@ -48,7 +37,7 @@ def check_address_mapbox(parts, session, key):
 			return result['features'][0]['text']
 	else: return 'Not Found'
 	
-def parse_response(result):
+def parse_google_response(result, parts):
 	#if google finds point
 	if result['status'] == 'OK':
 		for piece in reversed(result['results'][0]['address_components']):
@@ -66,16 +55,32 @@ def parse_response(result):
 	else:
 		return 'Not Found'
 
-key = open(args.key, 'r').read()
-with requests.Session() as google:
-	output = open(args.output, 'a')
-	with open(args.input, 'r') as source:
-		for row in source:
-			parts = (row.strip('\r\n')).split(',')
-			if parts[7] == 'None': output.write(','.join(parts + ['None']) + '\n')
-			elif args.mapbox: output.write(','.join(parts + [check_address_mapbox(parts, google, key)]) + '\n')
-			elif args.reverse: output.write(','.join(parts + [check_address_reverse(parts, google, key)]) + '\n')
-			elif args.zip: output.write(','.join(parts + [check_address_zip(parts, google, key)]) + '\n')
-			else: output.write(','.join(parts + [check_address(parts, google, key)]) + '\n')
+def write_test(inputF, outputF, keyF, reverse, zips, mapbox):
+	key = open(keyF, 'r').read()
+	with requests.Session() as google:
+		output = open(outputF, 'a')
+		with open(inputF, 'r') as source:
+			for row in source:
+				parts = (row.strip('\r\n')).split(',')
+				if parts[7] == 'None': output.write(','.join(parts + ['None']) + '\n')
+				elif mapbox: output.write(','.join(parts + [check_address_mapbox(parts, google, key)]) + '\n')
+				elif reverse: output.write(','.join(parts + [check_address_reverse(parts, google, key)]) + '\n')
+				elif zips: output.write(','.join(parts + [check_address_zip(parts, google, key)]) + '\n')
+				else: output.write(','.join(parts + [check_address(parts, google, key)]) + '\n')
+	output.close()	
 
-output.close()	
+if __name__ == '__main__':
+	
+	import argparse
+
+	#command line parser
+	parser = argparse.ArgumentParser(description = 'Checks an OpenAddresses formatted file against a web API')
+	parser.add_argument('input', help = 'OpenAddresses file with backfilled zip to check')
+	parser.add_argument('output', help = 'file to output accuracy data')
+	parser.add_argument('key', help = 'a file containing a valid api key')
+	parser.add_argument('-r', '--reverse', help = 'use a reverse geocode search', action = 'store_true')
+	parser.add_argument('-z', '--zip', help = 'search with zip code', action = 'store_true')
+	parser.add_argument('-m', '--mapbox', help = 'geocode from the mapbox API', action = 'store_true')
+	args = parser.parse_args()
+
+	write_test(args.input, args.output, args.key, args.reverse, args.zip, args.mapbox)
--- Zip_Backfill.py
+++ Zip_Backfill.py
@@ -1,18 +1,10 @@
 import fiona
-import argparse
 from shapely.geometry import shape
 from shapely.geometry import MultiPolygon
 from shapely.geometry import Point
 
-#command line parser
-parser = argparse.ArgumentParser(description = 'Backfills zip code to single file')
-parser.add_argument('input', help = 'OpenAddresses file to backfill from')
-parser.add_argument('output', help = 'file to to backfill to')
-parser.add_argument('shape_file', help = 'a census ZCTA shape file')
-args = parser.parse_args()
-
 #a search function that checks the last ZCTA found and then ZCTAs radiating out from it
-def find_zip(point, index):
+def find_zip(point, index, sorted_key, zips):
 	#set counters that move out from index of last found point
 	before = after = index
 	while before >= 0 or after < len(sorted_key):
@@ -37,32 +29,47 @@ def find_zip(point, index):
 	#if no ZCTA found
 	return('None', index)
 
-#construct list of zip polygons
-with fiona.drivers():
-	#open a shape file
-	with fiona.open(args.shape_file, 'r') as source:
-		#creating a list of polygons from the shape file
-		zips = MultiPolygon([shape(pol['geometry']) for pol in source])
-		#creating a list of tuples with the zip codes from the shape file and their order in the file
-		zips_key = [(pol['properties']['ZCTA5CE10'], inx) for inx, pol in enumerate(source)]
-		sorted_key = sorted(zips_key)
+def import_zip(shape_file):
+	#construct list of zip polygons
+	with fiona.drivers():
+		#open a shape file
+		with fiona.open(shape_file, 'r') as source:
+			#creating a list of polygons from the shape file
+			zips = MultiPolygon([shape(pol['geometry']) for pol in source])
+			#creating a list of tuples with the zip codes from the shape file and their order in the file
+			zips_key = [(pol['properties']['ZCTA5CE10'], inx) for inx, pol in enumerate(source)]
+			return (sorted(zips_key), zips)
 
 
-output = open(args.output, 'w')
-current_inx = 0
-with open(args.input, 'r') as points:
-	for point in points:
-		s_point = point.split(',')
-		if len(s_point) == 8: s_point.append('\r\n')
-		#check for zip
-		if len(s_point[7]) > 4 : output.write(point)
-		else:
-			#run function find_zip with lon and lat of current row as a point and the index of the last found point
-			result = find_zip(Point(float(s_point[0]), float(s_point[1])), current_inx)
-			#set current_inx to the index of the last point found
-			current_inx = result[1]
-			#zip of point is the zip of the found ZCTA
-			s_point[7] = result[0]
-			output.write(",".join(s_point) + '\n')
+def write_zip(outputF, inputF, sorted_key, zips):
+	output = open(outputF, 'w')
+	current_inx = 0
+	with open(inputF, 'r') as points:
+		for point in points:
+			s_point = point.split(',')
+			if len(s_point) == 8: s_point.append('\r\n')
+			#check for zip
+			if len(s_point[7]) > 4 : output.write(point)
+			else:
+				#run function find_zip with lon and lat of current row as a point and the index of the last found point
+				result = find_zip(Point(float(s_point[0]), float(s_point[1])), current_inx, sorted_key, zips)
+				#set current_inx to the index of the last point found
+				current_inx = result[1]
+				#zip of point is the zip of the found ZCTA
+				s_point[7] = result[0]
+				output.write(",".join(s_point) + '\n')
+	output.close()
 
-output.close()
+if __name__ == '__main__':
+
+	import argparse	
+	
+	#command line parser
+	parser = argparse.ArgumentParser(description = 'Backfills zip code to single file')
+	parser.add_argument('input', help = 'OpenAddresses file to backfill from')
+	parser.add_argument('output', help = 'file to to backfill to')
+	parser.add_argument('shape_file', help = 'a census ZCTA shape file')
+	args = parser.parse_args()
+
+	zips_load = import_zip(args.shape_file)
+	write_zip(args.output, args.input, zips_load[0], zips_load[1])
